e46fb75722bbd66c46ec7170ffd5e73765de696c
aakumykov <aakumykov@yandex.ru>
2026-06-01 13:13:42 +0300

    Перенёс тест RepeatFromToTest в файл repeatFromTo.kt

--- copy_between_streams_with_speed/src/androidTest/java/com/github/aakumykov/copy_between_streams_with_speed/utils/repeatFromTo.kt
+++ copy_between_streams_with_speed/src/androidTest/java/com/github/aakumykov/copy_between_streams_with_speed/utils/repeatFromTo.kt
@@ -1,5 +1,8 @@
 package com.github.aakumykov.copy_between_streams_with_speed.utils
 
+import org.junit.Assert
+import org.junit.Test
+
 fun repeatFromTo(
     from: Int,
     toUntil: Int,
@@ -10,5 +13,33 @@ fun repeatFromTo(
     while (i < toUntil) {
         action.invoke(i)
         i += step
+    }
+}
+
+
+class RepeatFromToTest {
+
+    @Test
+    fun repeat_from_1_to_10() {
+        val res = mutableListOf<Int>()
+        repeatFromTo(1,10) {
+            res.add(it)
+        }
+        Assert.assertEquals(
+            listOf(1,2,3,4,5,6,7,8,9),
+            res
+        )
+    }
+
+    @Test
+    fun repeat_from_0_to_10() {
+        val res = mutableListOf<Int>()
+        repeatFromTo(0,10) {
+            res.add(it)
+        }
+        Assert.assertEquals(
+            listOf(0,1,2,3,4,5,6,7,8,9),
+            res
+        )
     }
 }
