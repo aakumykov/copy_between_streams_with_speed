ec9f32da4613ca9efc9cba3e469b87fe3ec4ab33
aakumykov <aakumykov@yandex.ru>
2026-06-03 15:29:22 +0300

    copyBetweenStreamsWithSpeed3

---
+++ copy_between_streams_with_speed/src/main/java/com/github/aakumykov/copy_between_streams_with_speed/copyBetweenStreamsWithSpeed3.kt
@@ -0,0 +1,16 @@
+package com.github.aakumykov.copy_between_streams_with_speed
+
+import java.io.InputStream
+import java.io.OutputStream
+
+fun copyBetweenStreamsWithSpeed3(
+    inputStream: InputStream,
+    outputStream: OutputStream,
+    speedBytesPerSec: Int = -1,
+    stepsPerSecond: Int = 10,
+    logLevel: Int = 0,
+    tag: String = "CBSWS",
+    dataSize: Int? = null
+) {
+
+}
