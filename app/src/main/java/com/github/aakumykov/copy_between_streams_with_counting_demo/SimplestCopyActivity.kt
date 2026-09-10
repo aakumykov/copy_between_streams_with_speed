9119faa82a58760e8df9e8fd0bcb47226b06fad8
Andrey Kumykov <aakumykov@yandex.ru>
2026-09-10 23:35:48 +0300

    Что ж, понятно как использовать, но это неудобно.

--- app/src/main/java/com/github/aakumykov/copy_between_streams_with_counting_demo/SimplestCopyActivity.kt
+++ app/src/main/java/com/github/aakumykov/copy_between_streams_with_counting_demo/SimplestCopyActivity.kt
@@ -7,12 +7,17 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import com.github.aakumykov.copy_between_streams_with_counting_demo.databinding.ActivitySimplestCopyBinding
+import com.github.aakumykov.copy_between_streams_with_counting_demo.extensions.showToast
 import com.github.aakumykov.copy_between_streams_with_counting_demo.utils.random
 import com.github.aakumykov.copy_between_streams_with_speed.LimitedStreamCopier
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.launch
 import java.io.File
 import java.io.FileNotFoundException
+import kotlin.math.roundToInt
 
 class SimplestCopyActivity : AppCompatActivity() {
 
@@ -33,33 +38,52 @@ class SimplestCopyActivity : AppCompatActivity() {
 
     private val streamCopier by lazy { LimitedStreamCopier() }
 
+    private val dataSize = 1000
+
+    private var dataCopyingJob: Job? = null
+
     fun startCopy() {
         val sourceFile = File(cacheDir, "source_file.bin").apply { createNewFile() }
         val targetFile = File(cacheDir, "target_file.bin").apply { createNewFile() }
         if (!sourceFile.exists()) throw FileNotFoundException("source file does not exists")
         if (!targetFile.exists()) throw FileNotFoundException("target file does not exists")
 
-        val data = random.nextBytes(100)
+        val data = random.nextBytes(dataSize)
 
-        lifecycleScope.launch (Dispatchers.IO) {
+        dataCopyingJob = lifecycleScope.launch (Dispatchers.IO) {
 
             sourceFile.writeBytes(data)
 
             sourceFile.inputStream().use { inputStream ->
                 targetFile.outputStream().use { outputStream ->
 
+                    launch (Dispatchers.Main) {
+                        streamCopier
+                            .progressFlow
+                            .onCompletion {
+                                showToast("Скопировано")
+                            }
+                            .collect {
+                                showProgress(it)
+                            }
+                    }
+
                     streamCopier.copyFromStreamToStream(
                         inputStream,
                         outputStream,
-                        1000
+                        10
                     )
-                }.also {
-                    println("outputStream...")
-                }.also {
-                    println("inputStream ...")
-                }
 
+                    dataCopyingJob?.cancel()
+                    dataCopyingJob = null
+                }
             }
+
         }
+    }
+
+    fun showProgress(value: Long) {
+        val progress = ((1f * value / dataSize) * 100).roundToInt()
+        binding.progressBar.progress = progress
     }
 }
