f194b2804fd21df1bd724bd64cbb8b552c169852
Andrey Kumykov <aakumykov@yandex.ru>
2026-09-13 11:25:18 +0300

    Тот же тест, но на наборе данных разного размера.

--- copy_between_streams_with_speed/src/androidTest/java/com/github/aakumykov/copy_between_streams_with_speed/LimitedStreamCopierTest2.kt
+++ copy_between_streams_with_speed/src/androidTest/java/com/github/aakumykov/copy_between_streams_with_speed/LimitedStreamCopierTest2.kt
@@ -1,30 +1,51 @@
 package com.github.aakumykov.copy_between_streams_with_speed
 
+import com.github.aakumykov.copy_between_streams_with_speed.ext.roundToFloatingDigits
 import com.github.aakumykov.copy_between_streams_with_speed.stream2stream_copier.LimitedStreamCopier
+import com.github.aakumykov.copy_between_streams_with_speed.utils.humanDecimalPlaces
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert
 import org.junit.Test
+import kotlin.math.roundToLong
 
 class LimitedStreamCopierTest2 : TestBase() {
 
     @Test
     fun `продолжительность_копирования_плюс_минус_20_от_расчётной`() = runTest {
-        val dataSize = 1000
-        prepareSourceAndTargetFiles(dataSize)
+        for (dataSize in 1..100) {
+            prepareSourceAndTargetFiles(dataSize)
 
-        val lsc = LimitedStreamCopier(
-            speedBytesPerSecond = 200,
-            progressRatePerSecond = 1,
-            dataCopyStepsPerSecond = 10
-        )
+            val speed = 200
+            val rate = 1
+            val steps = 10
 
-        val startTime = System.currentTimeMillis()
-        lsc.copyFromStreamToStream(
-            sourceFileStream,
-            targetFileStream
-        )
-        val duration = System.currentTimeMillis() - startTime
+            val estimatedDuration
+                    = ((1f * dataSize / speed)*1_000_000)
+                .roundToLong()
 
-        Assert.assertTrue("$duration in 4800..5200", duration in 4800..5200)
+            val lsc = LimitedStreamCopier(
+                speedBytesPerSecond = speed,
+                progressRatePerSecond = rate,
+                dataCopyStepsPerSecond = steps
+            )
+
+            val startTime = currentTime
+            lsc.copyFromStreamToStream(
+                sourceFileStream,
+                targetFileStream
+            )
+            val duration = currentTime - startTime
+
+            println("sz: $dataSize, " +
+                    "sp: $speed, " +
+                    "st: $steps " +
+                    "-> " +
+                    "es:${estimatedDuration.humanDecimalPlaces}, " +
+                    "dr:${duration.humanDecimalPlaces}")
+//            Assert.assertTrue("$duration in 4800..5200 при размере данных $dataSize", duration in 4800..5200)
+        }
     }
+
+    private val currentTime: Long
+        get() = System.currentTimeMillis() + System.nanoTime()
 }
