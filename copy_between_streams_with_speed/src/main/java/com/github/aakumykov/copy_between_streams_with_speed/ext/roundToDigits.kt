08b513c9012d901caf1aa8721df6de287d759175
Andrey Kumykov <aakumykov@yandex.ru>
2026-09-13 11:24:57 +0300

    Float.roundToFloatingDigits() возвращает Float вместо String

--- copy_between_streams_with_speed/src/main/java/com/github/aakumykov/copy_between_streams_with_speed/ext/roundToDigits.kt
+++ copy_between_streams_with_speed/src/main/java/com/github/aakumykov/copy_between_streams_with_speed/ext/roundToDigits.kt
@@ -2,12 +2,12 @@ package com.github.aakumykov.copy_between_streams_with_speed.ext
 
 import java.util.Locale
 
-internal fun Float.roundToFloatingDigits(n: Int): String {
+internal fun Float.roundToFloatingDigits(n: Int): Float {
     return String.format(
         Locale.getDefault(),
         "%.${n}f",
         this
-    )
+    ).toFloat()
 }
 
 internal fun Double.roundToFloatingDigits(n: Int): String {
