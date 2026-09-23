df5df67be3f4ec2164d6a12bfe1459583a22d726
Andrey Kumykov <aakumykov@yandex.ru>
2026-09-23 06:50:46 +0300

    Сделал Unit-тест в результате которого выяснил, что при скорости копирования, большей размера данных, реальная скорость падает вдвое.

---
+++ copy_between_streams_with_speed/src/test/java/android/util/Log.java
@@ -0,0 +1,30 @@
+package android.util;
+
+// Source - https://stackoverflow.com/a/46793567
+// Posted by Paglian, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-09-23, License - CC BY-SA 4.0
+
+public class Log {
+    public static int d(String tag, String msg) {
+        System.out.println("DEBUG: " + tag + ": " + msg);
+        return 0;
+    }
+
+    public static int i(String tag, String msg) {
+        System.out.println("INFO: " + tag + ": " + msg);
+        return 0;
+    }
+
+    public static int w(String tag, String msg) {
+        System.out.println("WARN: " + tag + ": " + msg);
+        return 0;
+    }
+
+    public static int e(String tag, String msg) {
+        System.out.println("ERROR: " + tag + ": " + msg);
+        return 0;
+    }
+
+    // add other methods if required...
+}
+
---
+++ copy_between_streams_with_speed/src/test/java/com/github/aakumykov/copy_between_streams_with_speed/LimitedStreamCopierUnitTest.kt
@@ -0,0 +1,87 @@
+package com.github.aakumykov.copy_between_streams_with_speed
+
+import android.util.Log
+import com.github.aakumykov.copy_between_streams_with_speed.stream2stream_copier.LimitedStreamCopier
+import com.github.aakumykov.copy_between_streams_with_speed.utils.random
+import org.junit.Assert
+import org.junit.Test
+import java.io.File
+import java.io.InputStream
+import java.io.OutputStream
+import kotlin.math.roundToLong
+
+class LimitedStreamCopierUnitTest {
+
+    @Test
+    fun simple_copy_test() {
+        val base = 1024
+
+        val dataSize = base
+        val speed = base * 2
+        val rate = 1
+
+        prepareSourceAndTargetFiles(dataSize)
+
+        val startTime = System.currentTimeMillis()
+        LimitedStreamCopier(speed, rate)
+            .copyFromStreamToStream(sourceFileStream, targetFileStream)
+        val duration = System.currentTimeMillis() - startTime
+
+        val estimatedTime = (1000f * dataSize / speed).roundToLong()
+        val diffPercents = (1f * duration - estimatedTime) / estimatedTime
+
+        Log.d(TAG, "--------------------------------------------------------------------")
+        Log.d(TAG, "dataSize:$dataSize, speed:$speed, est:$estimatedTime, real:$duration (${diffPercents})%")
+        Log.d(TAG, "--------------------------------------------------------------------")
+
+        Assert.assertTrue(sourceFile.exists())
+        Assert.assertTrue(targetFile.exists())
+
+        Assert.assertEquals(dataSize.toLong(), sourceFile.length())
+        Assert.assertEquals(dataSize.toLong(), targetFile.length())
+
+        Assert.assertEquals(sourceFileData, targetFileData)
+    }
+
+    private fun prepareSourceAndTargetFiles(sizeBytes: Int) {
+        prepareTempDir()
+        deleteFile(sourceFile)
+        deleteFile(targetFile)
+        writeRandomBytesToFile(sourceFile, sizeBytes)
+    }
+
+    private fun prepareTempDir() {
+        val tempDir = File(TEMP_DIR_NAME)
+        if (!tempDir.exists()) Assert.assertTrue(tempDir.mkdirs())
+    }
+
+    private fun deleteFile(file: File) {
+        file.delete()
+        Assert.assertFalse(file.exists())
+    }
+
+    private fun writeRandomBytesToFile(file: File, sizeBytes: Int) {
+        val pieceSize = DEFAULT_BUFFER_SIZE
+        val mainSteps = sizeBytes / pieceSize
+        repeat(mainSteps) {
+            file.appendBytes(random.nextBytes(pieceSize))
+        }
+        val mainSize = pieceSize * mainSteps
+        val additionalSize = sizeBytes - mainSize
+        file.appendBytes(random.nextBytes(additionalSize))
+        Assert.assertEquals(sizeBytes.toLong(), file.length())
+    }
+
+    companion object {
+        val TAG: String = LimitedStreamCopierUnitTest::class.java.simpleName
+        private const val TEMP_DIR_NAME = "test_temp_dir"
+        private const val SOURCE_FILE_NAME = "source.file"
+        private const val TARGET_FILE_NAME = "target.file"
+        private val sourceFile = File(TEMP_DIR_NAME, SOURCE_FILE_NAME)
+        private val targetFile = File(TEMP_DIR_NAME, TARGET_FILE_NAME)
+        private val sourceFileStream: InputStream get() = sourceFile.inputStream()
+        private val targetFileStream: OutputStream get() = targetFile.outputStream()
+        private val sourceFileData: String get() = sourceFile.readBytes().joinToString("")
+        private val targetFileData: String get() = targetFile.readBytes().joinToString("")
+    }
+}
